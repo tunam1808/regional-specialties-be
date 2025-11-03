4cd9d2d98b03f58182ae87fe6bc9d43180f1abe0
domai123 <dmai45530@gmail.com>
2025-11-03 22:15:37 +0700

    addimg

--- src/database.ts
+++ src/database.ts
@@ -2,6 +2,15 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
+dotenv.config();
+console.log("🔍 Database config:");
+console.log({
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USER: process.env.DB_USER,
+  DB_PASS: process.env.DB_PASS ? "**** (đã có)" : "(rỗng)",
+  DB_NAME: process.env.DB_NAME,
+});
 
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
